dc530e4d94851b7270e98c1115179a90a9ae0575
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2024-05-16 20:57:47 +0200

    menu with grabbable in session scene

--- VR_Oculus_Basics/Assets/testbutton.cs
+++ VR_Oculus_Basics/Assets/testbutton.cs
@@ -3,24 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
 
 public class testbutton : MonoBehaviour
 {
-    private bool state = false;
-
+    private bool state = true;
     [SerializeField]
     private TextMeshPro text;
-
     [SerializeField]
     private Material material;
-
-    public void test()
-    {
-        Debug.Log("Aldo");
-    }
-
-
-    public void changeText()
+    [SerializeField]
+    private GameObject toGrab;
+    public void changeState()
     {
         this.state = ! this.state;
 
@@ -28,13 +22,20 @@ public class testbutton : MonoBehaviour
         {
             this.text.text = "Grabbable";
             this.material.color= new Color(0f, 255f, 0f);
-
+            this.toGrab.GetComponent<Grabbable>().enabled = true;
+            this.toGrab.GetComponent<PointableUnityEventWrapper>().enabled = true;
+            this.toGrab.GetComponentInChildren<GrabInteractable>().enabled = true;
+            this.toGrab.GetComponentInChildren<HandGrabInteractable>().enabled = true;
         }
         else
         {
             this.text.text = "Denied Grabbable";
             this.material.color = new Color(255f, 0f, 0f);
-
+            this.toGrab.GetComponent<Grabbable>().enabled = false;
+            this.toGrab.GetComponent<PointableUnityEventWrapper>().enabled = false;
+            this.toGrab.GetComponentInChildren<GrabInteractable>().enabled = false;
+            this.toGrab.GetComponentInChildren<HandGrabInteractable>().enabled = false;
         }
     }
+    
 }
